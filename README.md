321b48df9459c25feb166fbf75249d98d3ea8d11
Ashwany Aggarwal <ashwany.mittal@gmail.com>
2023-12-31 01:44:52 +0530

    updated the advanced calculator and its now working

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Calculator
 
-This is a calculator made using flask API where it has 2 types. Simple calculator only used for simple calculations(add, subtract, multiply, divide) and Advanced calculator is not yet completed.
+This project was made to learn Flask api using python. 
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request
 
 from datetime import datetime
+import math
+
 
 app = Flask(__name__)
 
@@ -38,9 +40,38 @@ def calculate():
         if second_number == 0:
             result = "You can't divide by zero"
         else:
-            result = round(first_number/second_number,3)
+            result = round(first_number/second_number,5)
+    elif operation == 'power':
+        result = first_number**second_number
+    
 
     return render_template('simple.html', result=result)
+
+@app.route("/calculate_advanced", methods = ['post'])
+def calculate_advanced():
+    first_number = float(request.form['Number'])
+    operation = request.form['operation']
+
+
+    if operation == "sin":
+        result = math.sin(first_number)
+    
+    elif operation == "cos":
+        result = math.cos(first_number)
+
+    elif operation == "tan":
+        result = math.tan(first_number)
+
+    elif operation == "log10":
+        result = math.log10(first_number)
+
+    elif operation == "loge":
+        result = math.log(first_number)
+    
+    elif operation == "sqrt":
+        result = math.sqrt(first_number)
+
+    return render_template('advanced.html', result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
